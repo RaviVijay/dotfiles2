00939830732ae4379d9ab13352a30d580c29ebaa
RaviVijay <reeves.skr@gmail.com>
2018-01-03 16:11:31 -0800

    source in aws.

--- source_in_aws.sh
+++ source_in_aws.sh
@@ -4,4 +4,19 @@ cp dotfiles2/aliases ~/.aliases
 cp dotfiles2/tmux.conf ~/.tmux.conf
 cp dotfiles2/tmuxp.yaml ~/tmuxp.yaml
 
+source ~/.aliases
+
+mkdir -p Kaggle/data
+cd Kaggle/data
+alias kgd="kg download -u ravivijay -p Selvijay5 -c "
+mkdir rossmann-store-sales; 
+cd rossmann-store-sales; 
+kgd rossmann-store-sales
+cd ../
+
+mkdir favorita-grocery-sales-forecasting; 
+cd favorita-grocery-sales-forecasting; 
+kgd favorita-grocery-sales-forecasting
+cd ../
+
 
